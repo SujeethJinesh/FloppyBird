1667070141108d6fa9e25508b212a6c60f3888e2
SujeethJinesh <sujeethjinesh@gmail.com>
2022-10-03 23:01:27 -0700

    Add Parallax effect

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,10 +2,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int spriteIndex;
+
     private Vector3 direction;
 
     public float gravity = -9.8f;
     public float strength = 5f;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite),  0.15f, 0.15f);
+    }
 
     private void Update()
     {
@@ -31,5 +46,17 @@ public class Player : MonoBehaviour
         // TODO: This could probably be moved to fixedUpdate
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
+    }
+
+    private void AnimateSprite()
+    {
+        spriteIndex++;
+
+        if (spriteIndex >= sprites.Length)
+        {
+            spriteIndex = 0;
+        }
+
+        spriteRenderer.sprite = sprites[spriteIndex];
     }
 }
